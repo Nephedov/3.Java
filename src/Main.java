307020001f687ee6c8d23cd127be1538e5f5bab7
Mihail Nephedov <Nephedov93@gmail.com>
2022-09-17 05:32:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main (String [] args) {
+
+        int x = 100;
+        int y = 100;
+        int z;
+
+        if (y > 1000) {
+            z = y / 100;
+        } else {
+            z = 0;
+        }
+
+        System.out.println(x + y +z);
+
+    }
+}
